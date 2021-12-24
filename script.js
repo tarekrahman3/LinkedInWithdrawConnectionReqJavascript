8b7e4d0f51403ba1344258e99aab1852fd11e726
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-12-25 04:04:44 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+let buttons = document.getElementsByClassName('artdeco-button__text');
+ for(var i=30;i<buttons.length;i++){
+buttons[i].parentElement.click(); await new Promise(resolve => setTimeout(resolve, 2000));;
+document.getElementById("artdeco-modal-outlet").querySelectorAll(".artdeco-button__text")[2].click(); await new Promise(resolve => setTimeout(resolve, 2000));;
+}
