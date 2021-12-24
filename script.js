9be62ed50ade99db3cb011ccf8ffc298a4cf1c5f
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-12-25 04:23:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let buttons = document.getElementsByClassName('artdeco-button__text');
-for(var i=30;i<buttons.length;i++){
+for (var i=Number(buttons/2).toFixed(); i<buttons.length; i++){
     buttons[i].parentElement.click();
     await new Promise(resolve => setTimeout(resolve, 2000));
     document.getElementById("artdeco-modal-outlet").querySelectorAll(".artdeco-button__text")[2].click();
